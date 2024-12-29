6da1f42916ba4762e11063d2e4b80143bf7834b8
Otabekcdr <boltaboyevotabek207@gmail.com>
2024-12-29 13:56:47 +0500

    landing page

--- app.js
+++ app.js
@@ -1,23 +1,37 @@
-var swiper = new Swiper(".mySwiper", {
-    slidesPerView: 1,
-    spaceBetween: 10,
-    navigation: {
-        nextEl: ".swiper-next",
-        prevEl: ".swiper-prev",
-    },
-    breakpoints: {
-        640: {
-            slidesPerView: 1,
-            spaceBetween: 20,
+let loader = document.querySelector(".loader"),
+    body = document.querySelector("body")
+window.addEventListener("load", () => {
+    loader.classList.add("loader_active")
+    let interval = setInterval(removeLoader, 300)
+
+    function removeLoader() {
+        body.removeChild(loader)
+        clearInterval(interval)
+    }
+
+    AOS.init()
+
+    var swiper = new Swiper(".mySwiper", {
+        slidesPerView: 1,
+        spaceBetween: 10,
+        navigation: {
+            nextEl: ".swiper-next",
+            prevEl: ".swiper-prev",
         },
-        768: {
-            slidesPerView: 2,
-            spaceBetween: 40,
+        breakpoints: {
+            640: {
+                slidesPerView: 1,
+                spaceBetween: 20,
+            },
+            768: {
+                slidesPerView: 2,
+                spaceBetween: 40,
+            },
+            1024: {
+                slidesPerView: 3,
+                spaceBetween: 50,
+            },
         },
-        1024: {
-            slidesPerView: 3,
-            spaceBetween: 50,
-        },
-    },
-    loop: true,
+        loop: true,
+    })
 })
